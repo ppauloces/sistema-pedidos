11c048fe6cc88889aaa13ca079192fa36bfa3cf9
Paulo <ppauloces27@gmail.com>
2022-02-11 22:19:38 -0300

    1

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai+Looped:wght@300&family=Poppins:wght@300;400;500;700;800&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body style="font-family: 'Poppins', sans-serif;">
+
+    <!-- NAVBAR PRINCIPAL -->
+    <div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper" style="background-color: #d84315;">
+            <div class="">
+                <img src="img/post.png" alt="" width="160px"  align="center" style="padding-left: 110px; padding-bottom: 5px">
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+
+                <ul class="right hide-on-med-and-down">
+                    <li><a class="waves-effect btn-small" style="border-radius: 20px; background-color: white; color: #000000;">Lanchonetes disponíveis</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </div>
+
+    <!-- NAVBAR MOBILE -->
+    <ul class="sidenav" id="mobile-demo" style="background-color: #d84315;">
+    <br>
+        <li><a class="waves-effect btn-small" style="border-radius: 20px; background-color: white; color: #000000;">Lanchonetes disponíveis</a></li>
+    </ul>
+
+     <!-- SESSÃO 1 -->
+    <div class="center" style="padding-top: 8rem; background-color: #fff; padding-bottom: 15rem">
+        <h1 style="font-weight: 600; font-size: 80px; color: black;">Está com fome?</h1>
+        <span style="color: black; font-size: 18px;">Facilitamos sua forma de resolver isso. <br> Só clicando alguns botões.</span><br>
+        <a class="waves-effect btn-small" style="border-radius: 50px; background-color: black ; color: white; margin-top: 30px;font-size: bold;">ENCHER A PANÇA</a>
+    </div>
+    <!-- SESSÃO 2 -->
+    <div class="center" style="padding-top: 8rem; background-color: #ef9a9a; padding-bottom: 15rem">
+        <h1 style="font-weight: 600; font-size: 50px; color: ffd600 ;">As melhores Lanchonetes da sua <br> cidade aqui!</h1>
+        <span style="color: ffd600 ; font-size: 18px;">Dezenas de lanches e bebidas para explorar. E<br> nem precisa passar perrengue.</span><br>
+    </div>
+
+
+    
+    <div class="row" style="padding-top: 40px;">
+    <div class="col s12 m6 l4">
+      <div class="card" style="padding: 70px;">
+        <div class="card-image" style="border-style: solid">
+          <img src="img/hamburguer.jpg">
+        </div>
+        <div class="card-content">
+          <h4>Hamburguer do Ney</h4>
+        </div>
+        <div class="card-action">
+        <a href="http://www.freetimelearning.com" target="_blank" class="btn" style="background-color: #d84315">Visitar lanchonete</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m6 l4">
+      <div class="card" style="padding: 70px;">
+        <div class="card-image" style="border-style: solid">
+          <img src="img/salgado.jpg">
+        </div>
+        <div class="card-content">
+          <h4>Lanchonete da Ivone</h4>
+        </div>
+        <div class="card-action">
+        <a href="http://www.freetimelearning.com" target="_blank" class="btn" style="background-color: #d84315">Visitar lanchonete</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m6 l4">
+      <div class="card" style="padding: 70px;">
+        <div class="card-image" style="border-style: solid">
+          <img src="img/acaraje.jpg">
+        </div>
+        <div class="card-content">
+          <h4>Acarajé do João</h4>
+        </div>
+        <div class="card-action">
+        <a href="http://www.freetimelearning.com" target="_blank" class="btn" style="background-color: #d84315">Visitar lanchonete</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m6 l4">
+      <div class="card" style="padding: 70px;">
+        <div class="card-image" style="border-style: solid">
+          <img src="img/hamburguer.jpg">
+        </div>
+        <div class="card-content">
+          <h4>Hamburguer do Ney</h4>
+        </div>
+        <div class="card-action">
+        <a href="http://www.freetimelearning.com" target="_blank" class="btn" style="background-color: #d84315">Visitar lanchonete</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m6 l4">
+      <div class="card" style="padding: 70px;">
+        <div class="card-image" style="border-style: solid">
+          <img src="img/hamburguer.jpg">
+        </div>
+        <div class="card-content">
+          <h4>Hamburguer do Ney</h4>
+        </div>
+        <div class="card-action">
+        <a href="http://www.freetimelearning.com" target="_blank" class="btn" style="background-color: #d84315">Visitar lanchonete</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col s12 m6 l4">
+      <div class="card" style="padding: 70px;">
+        <div class="card-image" style="border-style: solid">
+          <img src="img/hamburguer.jpg">
+        </div>
+        <div class="card-content">
+          <h4>Hamburguer do Ney</h4>
+        </div>
+        <div class="card-action">
+        <a href="http://www.freetimelearning.com" target="_blank" class="btn" style="background-color: #d84315">Visitar lanchonete</a>
+        </div>
+      </div>
+    </div>
+</div>
+
+  
+  
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('.sidenav').sidenav();
+        });
+
+    </script>
+</body>
+
+</html>
